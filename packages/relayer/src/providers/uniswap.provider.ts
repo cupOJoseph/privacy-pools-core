ebe9463c1e34e71ec116ca5114a3fa8b0c40fb93
casiojapi <casiopilled@gmail.com>
2025-05-08 10:58:56 -0300

    feat: add swap method to uniswap provider

--- packages/relayer/src/providers/uniswap.provider.ts
+++ packages/relayer/src/providers/uniswap.provider.ts
@@ -1,13 +1,17 @@
 import { Token } from '@uniswap/sdk-core'
 import { FeeAmount } from '@uniswap/v3-sdk'
-import { Address, getContract } from 'viem'
+import { Address, getContract, createWalletClient, http, Abi, encodeAbiParameters } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts';
+import { writeContract } from 'viem/actions';
 
+import { getChainConfig, getSignerPrivateKey } from "../config/index.js";
 import { web3Provider } from '../providers/index.js'
 import { BlockchainError, RelayerError } from '../exceptions/base.exception.js'
 import { isViemError } from '../utils.js'
-import { QUOTER_CONTRACT_ADDRESS, WRAPPED_NATIVE_TOKEN_ADDRESS } from './uniswap/constants.js'
+import { UNIVERSAL_ROUTER_ADDRESS, QUOTER_CONTRACT_ADDRESS, WRAPPED_NATIVE_TOKEN_ADDRESS } from './uniswap/constants.js'
 import { IERC20MinimalABI } from './uniswap/erc20.abi.js'
 import { QuoterV2ABI } from './uniswap/quoterV2.abi.js'
+import { UniversalRouterABI } from './uniswap/universalRouter.abi.js';
 
 export type UniswapQuote = {
   chainId: number;
@@ -67,7 +71,7 @@ export class UniswapProvider {
       }])
 
       // amount, sqrtPriceX96After, tickAfter, gasEstimate
-      const [amount, , , ] = quotedAmountOut.result;
+      const [amount, , ,] = quotedAmountOut.result;
       return {
         in: {
           amount: amountIn, decimals: tokenIn.decimals
@@ -84,7 +88,84 @@ export class UniswapProvider {
         throw RelayerError.unknown("Something went wrong while quoting")
       }
     }
-
   }
 
+  async swapExactInputSingle({
+    chainId,
+    amountIn,
+    quotedAmountOut,
+    slippageBps,
+    assetIn,
+    assetOut,
+    recipient
+  }: {
+    chainId: number;
+    amountIn: bigint;
+    quotedAmountOut: bigint;
+    slippageBps: bigint;
+    assetIn: Address;
+    assetOut: Address;
+    recipient: Address;
+  }): Promise<string> {
+    const config = getChainConfig(chainId);
+    const routerAddress = UNIVERSAL_ROUTER_ADDRESS[chainId.toString()];
+    if (!routerAddress) {
+      throw RelayerError.unknown(`Universal Router not configured for chain ${chainId}`);
+    }
+
+    const privateKey = getSignerPrivateKey(chainId);
+    const account = privateKeyToAccount(privateKey as `0x${string}`);
+
+    const client = createWalletClient({
+      chain: {
+        id: config.chain_id,
+        name: config.chain_name,
+        rpcUrls: { default: { http: [config.rpc_url] } },
+        nativeCurrency: config.native_currency!,
+      },
+      transport: http(config.rpc_url),
+      account,
+    });
+
+    const deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 10); // 10 minutes
+
+
+    // when setting slippage use this:
+    // const slippageBps = 10n; // 0.1%
+    const minAmountOut = quotedAmountOut - (quotedAmountOut * slippageBps / 10_000n);
+
+    // command: V3_SWAP_EXACT_IN
+    const commands = '0x00';
+
+    const encodedInput = encodeAbiParameters(
+      [
+        { name: 'tokenIn', type: 'address' },
+        { name: 'tokenOut', type: 'address' },
+        { name: 'fee', type: 'uint24' },
+        { name: 'recipient', type: 'address' },
+        { name: 'amountIn', type: 'uint256' },
+        { name: 'amountOutMin', type: 'uint256' },
+        { name: 'deadline', type: 'uint160' },
+      ],
+      [
+        assetIn,
+        assetOut,
+        FeeAmount.MEDIUM,
+        recipient,
+        amountIn,
+        minAmountOut,
+        deadline,
+      ]
+    );
+
+    const hash = await writeContract(client, {
+      address: routerAddress,
+      abi: UniversalRouterABI.abi,
+      functionName: 'execute',
+      args: [commands, [encodedInput]],
+      account
+    });
+
+    return hash;
+  }
 }
